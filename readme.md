f072355f3c920c06f20715bbc34953b45f0f6070
divit <supa.suterpartner@gmail.com>
2021-12-10 08:37:36 +0100

    initial release, v 1.0.0

--- readme.md
+++ readme.md
@@ -1,25 +1,30 @@
 # Wikipedia preview box (summary)
+
+Version 1.0.0, December 10, 2021, df
+
 Automatically turns any link to a Wikipedia article on your website into a link with a nice preview box (article summary). Add one line of code and it works. Tried to make it as simple as possible so that everyone can use it.
 
 ## Example
 Go to the [project page](https://su-pa.net/wikiPrevBox/) and ["Try it out"](https://su-pa.net/wikiPrevBox/).
 
 ## Features & characteristics
-- Lightweight; just 109 lines of JavaScript code, some CSS, a Wikipedia logo, a font and a bullet-gif
+- Lightweight; only 116 lines of JavaScript, some CSS, a one character 'Wikipedia-W' font, a font and a bullet gif
 - No installation needed: Insert one line of code and it works
 - It's like magic, paste a link to a Wikipedia article in your content and the preview box just pops up!
 - Works with touch & mouse, on mobile & desktop
 - Preview box finds automatically the best place to display on mobile & desktop, landscape & portrait mode
-- Wikipedia links with a preview box (summary) are visibly marked by a small Wikipedia logo (W)
-- Fine animation of the (flying:) preview boxes 
-- Works with all languages & mixed content
+- Wikipedia links with a preview box (summary) are nicely marked by a small Wikipedia logo that is made by a 1-char-font (!W:)
+- Works with all languages, different text directions (ltr & rtl) and even mixed content/preview boxes
+- Also works with existing offsite link icons
 - Integrated "auto hyphenation" (CSS) for all (available) languages
+- Fine animation of the (flying:) preview boxes 
 - Shows summery & image (if any) inside every preview box (default)\
 Possible settings:
   - Show inside all preview boxes only the summary text, no images
   - ... as above, but nevertheless show the image for a specific Wikipedia link
   - No preview box for a specific link
   - Vary the width of the preview boxes
+  - Set the color of the 'W' next to the Wikipedia link and others via CSS
 - Easy to use settings
 - Simple to use on any website, web app or to [integrate in WordPress](https://su-pa.net/wikiPrevBox/) (see "How to" on the project website) and other CMS
 - Works in all modern browsers
@@ -42,7 +47,7 @@ After the following step(s), all links to a Wikipedia article on your website wi
 ```
 <br>
 
-### **CUSTOMIZE** (optional)
+### **SETTINGS** (optional)
 ### **Affects all preview boxes** 
 are part of the ```<script ...>```-tag; currently you can set two parameters:
 >data-wikipreview="noimages"\
@@ -53,7 +58,7 @@ data-wikipreview="width=*value*"
 <script data-wikipreview="noimages,width=250" src="..."></script>
 ```
 - `noimages`  
-If this value is set, only the summary texts are displayed and no images; this can help reduce traffic in places with poor network conditions; or select only the images you like to display.
+If this value is set, only the summary texts   and no imagesareshown; this can help toreduce traffic in places with poor network conditions; or show only the images you like (by using`#showimage`,seebelow).
 - `width=...`  
  Width of the preview boxes, number of pixels, value between 200 and 400, default is 300.  
  <br>
@@ -70,15 +75,17 @@ are part of a Wikipedia link; currently you can set two parameters:
 - `#nopreview`  
 Does not show the Wikipedia preview box for a given link, but resets it to a common link to a Wikipedia article.
 - `#showimage`  
-Set this, if you have used `noimages` in the `<script ...>` tag (see above) but still want to show a picture in a specific Wikipedia preview box.
+Set this, if you have used `noimages` in the `<script ...>` tag (see above) but still want to show a picture in a specific Wikipedia preview box. This gives you the ability to take control of when an image is displayed inside a preview box.
 <br><br> 
 
-### **Changes via stylesheet**
-You can overwrite the CSS rules of the preview boxes in your own stylesheet.
-#### **Change the color of the Wikipedia links, e.g.**
-`a.wikiLink {color: darkgreen;}`		      /* *link color* \*/\
-`a:hover.wikiLink {color: orangered;}`  	/* *mouseover/fingertip* \*/\
-`a:active.wikiLink {color: orangered;}` 	/* *mouse onclick/fingertip* \*/\
+### **Customize via Stylesheet**
+You can overwrite the CSS rules of the preview boxes in your own stylesheet, e.g.
+
+`a.wikiLink::after {color: #212427 !important;}`		      /* *set the color of the 'W' next Wikipedia links, default is 'inherit'* \*/\
+`a.wikiLink::after {font-size: 0.8em !important;}`		      /* *size of the 'W', default is 0.7em* \*/\
+`a.wikiLink {color: darkgreen;}`		      /* *color of the Wikipedia link* \*/\
+
+... override any CSS rules you like to do; use `!important` if  necessary; you will find all rules in the stylesheet **wikiPreviewBox.css**
 
 ## About & privacy
 Get the information on the [project page](https://su-pa.net/wikiPrevBox/) 
@@ -86,7 +93,7 @@ Get the information on the [project page](https://su-pa.net/wikiPrevBox/)
 ## Future plans, ideas
 - Offline/cache features, prevent doing same things twice (reduce electron travels:)
 - Custom text lenght of (a specific) preview box(es)
-- simple font settings 
+- Simpler 'W' settings (color & size) without stylesheet 
 - Portrait modus on mobile: show image next to the summary text
 - ... 
 
--- wikiPrevBox/wikiPreviewBox.js
+++ wikiPrevBox/wikiPreviewBox.js
@@ -1,6 +1,6 @@
  /*!
  * WIKIPEDIA PREVIEW BOXES
- * su-pa.net (c) version 11/22/2021, nick, MIT-License
+ * su-pa.net (c) version 12/10/2021, 1.0.0, nick, MIT-License
  * https://su-pa.net/wikiPrevBox/ :: project page
  */
 //#region wikipedia preview boxes
@@ -10,50 +10,53 @@
     //path to the CSS file
     document.getElementsByTagName('head')[0].insertAdjacentHTML('beforeend', '<link rel="stylesheet" type="text/css" href="/wikiPrevBox/wikiPreviewBox.min.css">');
     //document.getElementsByTagName('head')[0].insertAdjacentHTML('beforeend','<link rel="stylesheet" type="text/css" href="https://su-pa.net/wikiPrevBox/wikiPreviewBox.min.css">');
-
-    //document.getElementsByTagName('head')[0].insertAdjacentHTML('beforeend','<link rel="stylesheet" type="text/css" href="/wikiPreviewBox.min.css">');
-
+    
     //#region previewbox
     //add an empty wiki-preview-box container, everything happens inside here 
     document.body.insertAdjacentHTML('afterbegin', '<article id="wikiPreviewBox" class="wikiPreviewBox"></article>');
 
     let showNoImages = false;
     let wikiBoxWidth = 300; //default preview box width
-    //additional custom settings
+    let textDirection = document.dir || ""; //website text direction if set (<html ... dir="rtl">)    
+
+    //optional custom settings affecting all preview boxes
+    //how to use: <script data-wikipreview="noimages,width=250" src="... ></script>    
     if (typeof (document.currentScript.dataset.wikipreview) != "undefined") {
         // show image in the preview boxes (if there is one); set in <script data-wikipreview="noimages" src="... ></script>
         showNoImages = document.currentScript.dataset.wikipreview.includes("noimages") ? true : false;        
-        //custom width for the preview boxes, standard width=300, min=200, max=400 
-        wikiBoxWidth = Math.min(Math.max(parseInt(document.currentScript.dataset.wikipreview.replace(/\D/g,'') || 300),200),400);
-        //how to use: <script data-wikipreview="width=250,noimages" src="... ></script>       
+        // custom width for the preview boxes, standard width=300, min=200, max=400 
+        wikiBoxWidth = Math.min(Math.max(parseInt(document.currentScript.dataset.wikipreview.replace(/\D/g,'') || 300),200),400);         
     }
 
-    //max number of characters in the wiki preview box
-    let wikiBoxMaxLenght = 280; //includes some HTML tags (~15... signs <p><b>..., + on disambiguation terms + <ul>, <li>..., ...)
-
+    let wikiBoxMaxLenght = 280; //maxnumof chars in the preview box (includes some HTML tags (~15 chars ... <p><b>... // on disambiguation terms additional <ul><li>...</li>...</ul>)
     let whatArticle = ""; //term to show (last part of the url to a wikipedia article)
     let wikiArticleUrl = "", wikiLang = "", wikiTermPos, wikiTerm;
 
     let wikiPreviewBox = document.getElementById("wikiPreviewBox");
     wikiPreviewBox.style.width = wikiBoxWidth + "px";
     
+    //add preview box events
     wikiPreviewBox.addEventListener("click", hideWikiBox);
     wikiPreviewBox.addEventListener("mouseover", showWikiBox);
     wikiPreviewBox.addEventListener("mouseout", hideWikiBox);
     //#endregion
 
-    //#region add events to wiki-links, some positioning of a preview box
+    //#region Wikipedia links
+    
     //get all wikipedia links exept the once that have a "#nopreview" at the end
     let allWikiLinks = document.querySelectorAll("[href*='wikipedia.org']:not([href*='#nopreview'])");
 
-    //"cleaning": if there are links with an added "#nopreview" => clean up the links & remove "#nopreview"
+    //clean the links with "#nopreview" at the end (remove it)
     let allWithNopreview = document.querySelectorAll("[href*='#nopreview']");
     for (let z = 0; z < allWithNopreview.length; z++) {
         allWithNopreview[z].href = allWithNopreview[z].href.replace('#nopreview','');
     }
 
-    for (let i = 0; i < allWikiLinks.length; i++) {
+    //make all Wikipedia links ready (for getting data, pos. the preview box, rtl) 
+    for (let i = 0; i < allWikiLinks.length; i++) {        
         allWikiLinks[i].classList.add('wikiLink');
+        if (textDirection==="rtl") {allWikiLinks[i].classList.add("wikiLink_rtl")} //if website direction is set (rtl) => beautify the Wikipedia links inside content
+
         allWikiLinks[i].addEventListener("mouseover", (e) => {
             wikiTerm = e.currentTarget;
             wikiArticleUrl = wikiTerm.href;
@@ -61,7 +64,6 @@
             wikiLang = wikiArticleUrl.split(".")[0].split("//")[1]; //get language :: en, zh, de, ...
 
             //#region position "left<>right" of the wiki preview box
-            //show wiki preview box below wiki-tag
             wikiTermPos = wikiTerm.getBoundingClientRect();
             let wikiBoxLeft = wikiTermPos.left - (wikiBoxWidth / 2) + ((wikiTermPos.right - wikiTermPos.left) / 2);
             if (wikiBoxLeft < 0) { wikiBoxLeft = 10; } //do not pos. it out of the left edge                    
@@ -70,9 +72,9 @@
             let winInnerWidth = window.innerWidth; 
             if (winInnerWidth < wikiBoxWidth) { wikiBoxLeft = winInnerWidth - wikiBoxWidth + (wikiBoxWidth - winInnerWidth); }
             else {
-                if ((wikiBoxLeft + wikiBoxWidth) > winInnerWidth) { wikiBoxLeft = winInnerWidth - wikiBoxWidth - 10; } //do not pos out right side
+                if ((wikiBoxLeft + wikiBoxWidth) > winInnerWidth) { wikiBoxLeft = winInnerWidth - wikiBoxWidth - 20; } //do not pos out right side
             }
-            wikiPreviewBox.style.left = wikiBoxLeft + "px"; //maybe move this (all) to the function in the bottom
+            wikiPreviewBox.style.left = wikiBoxLeft + "px"; //maybe move this (all, not on a monday) to the function in the bottom
             //#endregion
 
             getWikiPreviewData(whatArticle, wikiLang);
@@ -92,21 +94,21 @@
         if (showThisImgAnyway) { whatArticle = whatArticle.replace('#showimage','')}
 
         let arrArticleSummary = [];
-        //fetch/get preview data 
+        //fetch/get data for the preview box
         if (navigator.onLine) { //android webview could(?) show online despite of offline, 11/2021 (bug) 
             fetch('https://' + whatLang + '.m.wikipedia.org/api/rest_v1/page/summary/' + whatArticle)
                 .then(response => {                    
                     if (response.status >= 200 && response.status <= 299) { //ok, no network error (but neverthless could be a 404 error)
                         response.json().then(wikiSummData => {
-                            //console.log(wikiSummData); //all available summary data                            
+                            //console.log(wikiSummData); //all available summary data
                             //show image => wiki has an image && (showNoImages is not true || showNoImages is true but #showimage is set) ("#showimage" at the end of the link to the wiki article)
                             if ((wikiSummData.hasOwnProperty('thumbnail') && !showNoImages) || showThisImgAnyway) { 
                                 arrArticleSummary = ["<img height='" + wikiSummData.thumbnail.height + "' src='" + wikiSummData.thumbnail.source + "'/>", wikiSummData.extract_html, 'https://' + whatLang + '.wikipedia.org/wiki/' + whatArticle, wikiSummData.title];
-                            } 
-                            //show no image => there is no image on wiki or no image should be shown ()
+                            }
+                            //show no image => there is no image on wiki or no image should be shown
                             else { arrArticleSummary = ["", wikiSummData.extract_html, 'https://' + whatLang + '.wikipedia.org/wiki/' + whatArticle, wikiSummData.title]; } //no image
                             
-                            createWikiBox(arrArticleSummary);
+                            createWikiBox(arrArticleSummary,wikiSummData.dir);
                         })
                     }
                     else {/*network error/404, term not found (but there is still a(n "error") message: "GET https...wiki... 404" )*/ }
@@ -118,7 +120,7 @@
     //#endregion
 
     //#region create, hide, show preview-box
-    function createWikiBox(arrArticleSum) {
+    function createWikiBox(arrArticleSum,textDirection) {
         let wikiBoxContent = "";
         wikiBoxContent = arrArticleSum[0]; //thumbnail if any
 
@@ -133,15 +135,18 @@
             wikiBoxContent += arrArticleSum[1].substring(0, wikiBoxMaxLenght-3) : //-3: >could< end in a '</p' (preventively remove 3 chars)          
             wikiBoxContent += arrArticleSum[1].substring(0, wikiBoxMaxLenght-9);  //-9: same but '</li></ul', etc.? (rare, e.g. disambiguation link)
             if (wikiBoxContent.slice(-1) == ".") {wikiBoxContent = wikiBoxContent.substring(0, wikiBoxContent.length - 1);} //cosmetically, avoid ". ..." at the end, showing " ..." instead
-            wikiBoxContent += "&nbsp;..." + lastClosingTag; // +lastClosingTag: '</li>' is not needed because browsers close <li> automatically (faster) in this case 
+            wikiBoxContent += "&nbsp;..." + lastClosingTag; // +lastClosingTag: '</li>' is not needed because browsers close <li> automatically (faster) in this case & very rare case
         }
         wikiPreviewBox.innerHTML = wikiBoxContent;
 
-        let wikiBoxfooter = "<span class='wikiBoxfooter'><a href='" + arrArticleSum[2] + "' target='_blank' rel='noopener'>" + arrArticleSum[3] +
-            "</a> (wikipedia.org)</span><a href='https://su-pa.net/wikiPrevBox/' target='_blank' rel='noopener'><img class='wikiBoxLogo' src='https://su-pa.net/wikiPrevBox/wikiLogo.svg' title='about Wikipedia preview box - click for info'>";
-        wikiPreviewBox.insertAdjacentHTML('beforeend', wikiBoxfooter);     
-        wikiPreviewBox.getElementsByTagName("P")[0].setAttribute("lang", wikiLang); //set language for hyphens, CSS
+        let wikiBoxfooter = "<span class='wikiBoxfooter_" + textDirection + "'><a href='" + arrArticleSum[2] + "' target='_blank' rel='noopener'>" + arrArticleSum[3] + "</a> (wikipedia.org)</span>" +
+            "<a href='https://su-pa.net/wikiPrevBox/' target='_blank' rel='noopener' title='about Wikipedia preview box - click for info' alt='about Wikipedia preview box - click for info'>" +
+                "<span class='wikiBoxLogo-w_" + textDirection + "'>W</span></a>";
         
+        wikiPreviewBox.insertAdjacentHTML('beforeend', wikiBoxfooter);     
+        wikiPreviewBox.getElementsByTagName("P")[0].setAttribute("lang", wikiLang); //set language for hyphens (CSS)
+        wikiPreviewBox.getElementsByTagName("P")[0].setAttribute("dir", textDirection);
+
         showWikiBox();
     }
     function showWikiBox() {
